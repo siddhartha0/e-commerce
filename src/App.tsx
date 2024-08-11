84211b6627c8b6d4164d085f10b948d3f3bdf1f0
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-08-11 17:26:15 +0545

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex text-grey font-bold">Hello</div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      primary: "#EAEEF3",
+      secondary: "#004d96",
+      "primary-light": "#FFEEEF",
+      dark: "#1D3557",
+      white: "#FFF",
+      hold: "#f69400",
+      fav: "#fd7014",
+      danger: "#8c0101",
+      "light-grey": "#f4f4f4",
+      grey: "#717171",
+      complete: "#05c192",
+      love: "#E72929",
+      black: "#0c0c0c",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
